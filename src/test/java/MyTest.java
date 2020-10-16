eedf4f84db1d88ddeecb384e9cfa9efc8232d28e
yangming0 <732541833@qq.com>
2020-10-16 23:59:10 +0800

    Initial commit

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,17 @@
+import com.yang.pojo.Books;
+import com.yang.service.BookService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class MyTest {
+    @Test
+    public void test(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookServiceImpl = (BookService) context.getBean("BookServiceImpl");
+        for (Books books : bookServiceImpl.queryAllBook()) {
+            System.out.println(books);
+        }
+    }
+}
